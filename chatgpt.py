768b17f2a94ad81e50119f425c18d3dfc535ff1b
Mark Felea-Motet <markmotet@gmail.com>
2023-04-02 11:31:24 -0700

    Added commented out gpt-4 model.

--- chatgpt.py
+++ chatgpt.py
@@ -4,6 +4,7 @@ def send_to_chatgpt(api_key, message_list):
     openai.api_key = api_key
     response = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
+        # model="gpt-4",
         messages=message_list
     )
 
